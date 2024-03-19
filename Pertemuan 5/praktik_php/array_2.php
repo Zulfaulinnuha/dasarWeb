777f59eab962378643f3fe673f4fb0513db94783
Zulfa14 <zulfanuha016@gmail.com>
2024-03-19 12:10:38 +0700

    Praktikum 2

---
+++ Pertemuan 5/praktik_php/array_2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: 20px auto;
+        }
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $Dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili' => 'Malang',
+        'jenis_kelamin' => 'Perempuan'
+    ];
+
+    echo "<table>";
+    echo "<tr><th>Informasi Dosen</th><th></th></tr>";
+    foreach ($Dosen as $key => $value) {
+        echo "<tr><td>$key</td><td>$value</td></tr>";
+    }
+    echo "</table>";
+    ?>
+</body>
+</html>
